1a4b6ee5802cca4561518a26fd5cd38342d1bbbc
Alberto Lourenço <alberto.lourenco8@gmail.com>
2023-04-09 04:05:38 +0000

    no message

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "OrganicView",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "OrganicView",
+            targets: ["OrganicView"])
+    ],
+    targets: [
+        .target(
+            name: "OrganicView",
+            resources: [
+                .process("OrganicView.swift")
+            ]
+        )
+    ]
+)
